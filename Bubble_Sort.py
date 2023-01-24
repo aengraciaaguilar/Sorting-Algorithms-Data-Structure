d59e0e4bacd9e49edb1d1fd3dafc1edc49608f40
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:21:18 +0800

    utilizing the print statements in bubble sort

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -10,4 +10,13 @@ def bubble_sort(arr):
                 tmp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = tmp
+                print(arr)
+
+arr = [78, 62, 56, 27, 90, 65, 71, 87, 33, 86]
+print("My array:", arr)
+print("The output of the sorted array using bubble sort algorithm:", arr)
+print("\n")
+print("The process of bubble sort:")
+bubble_sort(arr)
+print("\n")
 
