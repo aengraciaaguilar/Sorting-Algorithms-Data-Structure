c79f4942c1d49a5b520c244dab64f92a04796085
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:44:18 +0800

    utilizing the second def function in quick sort

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -9,3 +9,7 @@ def quick_sort(arrays, left, right):
         quick_sort(arrays, left, partition_positions - 1)
         quick_sort(arrays, partition_positions + 1, right)
 
+def partition(arrays, left, right):
+    i = left
+    j = right - 1
+    the_pivot = arrays[right]
