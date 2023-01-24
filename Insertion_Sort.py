902be2c256568d60432469c45f2499226736c8a8
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:30:13 +0800

    utilizing the last print function in the insertion sort

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -11,3 +11,11 @@ def insertion_sort(arr):
             j = j - 1
         arr[j + 1] = anchor
         print(arr)
+
+arr = [78, 62, 56, 27, 90, 65, 71, 87, 33, 86]
+print("My array:", arr)
+print("The output of the sorted array using insertion sort algorithm:", arr)
+print("\n")
+print("The process of insertion sort:")
+insertion_sort(arr)
+print("\n")
