0e0982c8bbd4858053a659a37d2f66138c250dbf
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:29:07 +0800

    utilizing the loop statements with the print statement

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -6,3 +6,8 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         anchor = arr[i]
         j = i - 1
+        while j >= 0 and anchor < arr[j]:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = anchor
+        print(arr)
