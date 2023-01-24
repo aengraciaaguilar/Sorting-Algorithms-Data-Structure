2869da22bd04c709b1077bc9dfe15b9a9cab8b0b
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:36:24 +0800

    utilizing the second while loop in merge sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -22,4 +22,12 @@ def merge_sort(arr):
                 arr[k] = right[j]
                 j += 1
             k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
 
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
