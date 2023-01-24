429317320042ff2d9d3c74555fc7993bd7462a3f
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:35:45 +0800

    utilizing the first while loop method in merge sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -14,3 +14,12 @@ def merge_sort(arr):
         merge_sort(right)
 
         i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
