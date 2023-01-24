4d80b2bd85e7bb8add91c131351a87c6dc75dcee
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:43:31 +0800

    utilizing the first function in quick sort

---
+++ Quick_Sort.py
@@ -0,0 +1,11 @@
+# Laboratory Activity # 7
+# Name: Aengracia Aguilar
+# Course and Year: BSCOE 2-2
+
+
+def quick_sort(arrays, left, right):
+    if left < right:
+        partition_positions = partition(arrays, left, right)
+        quick_sort(arrays, left, partition_positions - 1)
+        quick_sort(arrays, partition_positions + 1, right)
+
