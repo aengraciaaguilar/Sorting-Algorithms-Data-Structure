629e75152d8ce47bf9a303ca58345edf28d0203d
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-25 02:20:25 +0800

    utilizing the for in range method

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -4,3 +4,10 @@
 
 def bubble_sort(arr):
     size = len(arr)
+    for i in range(size - 1):
+        for j in range(size - 1 - i):
+            if arr[j] > arr[j + 1]:
+                tmp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+
